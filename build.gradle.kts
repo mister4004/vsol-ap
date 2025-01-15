cee916391fbf1e6429c50131b6cf4d99c83142b2
mister4004 <63348448+mister4004@users.noreply.github.com>
2025-01-15 12:30:44 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,23 @@
+// build.gradle (корневой файл проекта)
+buildscript {
+    ext.kotlin_version = '1.8.21' // Убедитесь, что версия Kotlin совместима
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.1.1'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }

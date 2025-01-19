e569b3b97499c32232ad55b2e088bfd91469ec28
mister4004 <63348448+mister4004@users.noreply.github.com>
2025-01-19 05:19:19 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     }
     plugins {
         id("com.android.application") version "8.1.1"
-        id("org.jetbrains.kotlin.android") version "1.9.20" // Понизить до совместимой версии
+        id("org.jetbrains.kotlin.android") version "1.9.23" // Понизить до совместимой версии
     }
 }
 
